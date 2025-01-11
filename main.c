ff1b70751c4eecbb808e5b997c6031860d501bba
alexandreIorio <alexandre.iorio@heig-vd.ch>
2025-01-11 17:58:39 +0100

    feat: add parameter file

--- main.c
+++ main.c
@@ -1,47 +1,114 @@
 #include <getopt.h>
 #include "plague.h"
 #include "video_utils.h"
+#include <fstream>
+#include <sstream>
 
 using namespace std;
-void startTimer(timespec &start)
+
+const std::string parameterNames[] = { "population",
+				       "healthy_infection_probability",
+				       "immune_infection_probability",
+				       "death_probability",
+				       "initial_infected",
+				       "initial_immune_percent",
+				       "proximity",
+				       "world_height",
+				       "world_width" };
+
+bool generateParameterFile(const std::string &filename, const Plague &p)
 {
-	clock_gettime(CLOCK_MONOTONIC, &start);
+	std::ofstream file(filename);
+	if (!file.is_open()) {
+		cerr << "Error: Could not create file " << filename << endl;
+		return false;
+	}
+
+	file << parameterNames[0] << " " << p.populationPercent << "\n";
+	file << parameterNames[1] << " " << p.healthyInfectionProbability
+	     << "\n";
+	file << parameterNames[2] << " " << p.immuneInfectionProbability
+	     << "\n";
+	file << parameterNames[3] << " " << p.deathProbability << "\n";
+	file << parameterNames[4] << " " << p.initialInfected << "\n";
+	file << parameterNames[5] << " " << p.immunePercent << "\n";
+	file << parameterNames[6] << " " << p.proximity << "\n";
+	file << parameterNames[7] << " " << p.worldHeight << "\n";
+	file << parameterNames[8] << " " << p.worldWidth << "\n";
+
+	file.close();
+	cout << "Parameters written to " << filename << endl;
+	return true;
 }
 
-double stopTimer(const timespec &start)
+bool loadParametersFromFile(const std::string &filename, Plague &p)
 {
-	timespec end;
-	clock_gettime(CLOCK_MONOTONIC, &end);
+	std::ifstream file(filename);
+	if (!file.is_open()) {
+		cerr << "Error: Could not open file " << filename << endl;
+		return false;
+	}
 
-	// Calcul de la différence en ms
-	double elapsed = (end.tv_sec - start.tv_sec) +
-			 (end.tv_nsec - start.tv_nsec) / 1e6;
-	return elapsed;
+	std::string line;
+	while (std::getline(file, line)) {
+		std::istringstream iss(line);
+		std::string key;
+		int value;
+		if (iss >> key >> value) {
+			if (key == parameterNames[0])
+				p.populationPercent = value;
+			else if (key == parameterNames[1])
+				p.healthyInfectionProbability = value;
+			else if (key == parameterNames[2])
+				p.immuneInfectionProbability = value;
+			else if (key == parameterNames[3])
+				p.deathProbability = value;
+			else if (key == parameterNames[4])
+				p.initialInfected = value;
+			else if (key == parameterNames[5])
+				p.immunePercent = value;
+			else if (key == parameterNames[6])
+				p.proximity = value;
+			else if (key == parameterNames[7])
+				p.worldHeight = value;
+			else if (key == parameterNames[8])
+				p.worldWidth = value;
+			else {
+				cerr << "Warning: Unknown key '" << key
+				     << "' in file " << filename << endl;
+			}
+		}
+	}
+	file.close();
+	return true;
 }
 
-void printUsage(Plague p)
+void printUsage(const Plague &p)
 {
-	cout << "Usages: Plague Simulator [options]\n"
-	     << "Options:\n"
-	     << "  -p, --population     Percent of population, default "
-	     << p.populationPercent << "%\n"
-	     << "  -m, --popilation-immune       Number of initial immune, default "
-	     << p.immunePercent << "%\n"
-	     << "  -e, --heathy-infection       Probability to infect a healthy person , default "
-	     << p.healthyInfectionProbability << "\n"
-	     << "  -n, --immune-infection       Probability to infect an immune person , default "
-	     << p.immuneInfectionProbability << "\n"
-	     << "  -r, --dead-probability       Probability of death, default "
-	     << p.deathProbability << "\n"
-	     << "  -i, --initial-infected       Number of initial infected, default "
-	     << p.initialInfected << "\n"
-	     << "  -y, --proximity      Proximity of infection, default "
-	     << p.proximity << "\n"
-	     << "  -h, --world-height  height of the grid, default "
-	     << p.worldHeight << "\n"
-	     << "  -w, --world-width   width of the grid, default "
-	     << p.worldWidth << "\n"
-	     << "--help           Display this information\n";
+	std::cout
+		<< "Usage: Plague Simulator [options]\n"
+		<< "Options:\n"
+		<< "  -p, --population <value>               Percent of population (default: "
+		<< p.populationPercent << "%)\n"
+		<< "  -m, --population-immune <value>        Percent of initial immune (default: "
+		<< p.immunePercent << "%)\n"
+		<< "  -e, --healthy-infection <value>        Probability to infect a healthy person (default: "
+		<< p.healthyInfectionProbability << "%)\n"
+		<< "  -n, --immune-infection <value>         Probability to infect an immune person (default: "
+		<< p.immuneInfectionProbability << "%)\n"
+		<< "  -r, --dead-probability <value>         Probability of death (default: "
+		<< p.deathProbability << "%)\n"
+		<< "  -i, --initial-infected <value>         Number of initial infected (default: "
+		<< p.initialInfected << ")\n"
+		<< "  -y, --proximity <value>                Proximity of infection (default: "
+		<< p.proximity << ")\n"
+		<< "  -h, --world-height <value>             Height of the grid (default: "
+		<< p.worldHeight << ")\n"
+		<< "  -w, --world-width <value>              Width of the grid (default: "
+		<< p.worldWidth << ")\n"
+		<< "  -f, --file <file>                      Load parameters from file\n"
+		<< "  -g, --generate-file                    Generate a parameter file\n"
+		<< "      --help                             Display this information\n";
 }
 
 int main(int argc, char *argv[])
@@ -60,7 +127,7 @@ int main(int argc, char *argv[])
 	p.initialInfected = 1;
 	p.proximity = 2;
 
-	const char *shortOptions = "p:e:n:d:r:i:m:y:h:w:v:";
+	const char *shortOptions = "p:e:n:d:r:i:m:y:h:w:vf:g";
 	const struct option longOptions[] = {
 		{ "population", required_argument, nullptr, 'p' },
 		{ "healthy-infection-probability", required_argument, nullptr,
@@ -74,6 +141,9 @@ int main(int argc, char *argv[])
 		{ "world-height", required_argument, nullptr, 'h' },
 		{ "world-width", required_argument, nullptr, 'w' },
 		{ "video", no_argument, nullptr, 'v' },
+		{ "file", required_argument, nullptr, 'f' },
+		{ "generate-file", no_argument, nullptr,
+		  'g' }, // Nouvelle option
 		{ "help", no_argument, nullptr, ' ' },
 		{ nullptr, 0, nullptr, 0 }
 	};
@@ -125,6 +195,20 @@ int main(int argc, char *argv[])
 		case 'w':
 			p.worldWidth = atoi(optarg);
 			break;
+		case 'f': {
+			std::string filename = optarg;
+			if (!loadParametersFromFile(filename, p)) {
+				return 1; // Erreur si le fichier ne peut pas être lu
+			}
+			break;
+		}
+		case 'g': {
+			std::string filename = "parameters.txt";
+			if (!generateParameterFile(filename, p)) {
+				return 1; // Erreur si le fichier ne peut pas être créé
+			}
+			exit(0);
+		}
 		case 'v':
 			doVideo = true;
 			break;
