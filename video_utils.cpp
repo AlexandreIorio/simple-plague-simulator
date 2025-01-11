a10f82219af754ff76906a6b6f7cd5a7506a5bd3
André <andre_miguel_costa@hotmail.com>
2025-01-12 00:27:42 +0100

    refactor: use c api

---
+++ video_utils.cpp
@@ -0,0 +1,65 @@
+#include <vector>
+#include <opencv4/opencv2/opencv.hpp>
+#include "video_utils.h"
+
+cv::Scalar stateToColor(int state)
+{
+	switch (state) {
+	case 0:
+		return cv::Scalar(255, 255, 255); // Blanc
+	case 1:
+		return cv::Scalar(0, 128, 0); // Vert
+	case 2:
+		return cv::Scalar(0, 0, 128); // Rouge
+	case 3:
+		return cv::Scalar(0, 0, 0); // Noir
+	case 4:
+		return cv::Scalar(128, 128, 0);
+	default:
+		return cv::Scalar(255, 255, 255); // Par défaut blanc
+	}
+}
+
+cv::Mat createFrame(int *grid, size_t w, size_t h, int cellSize)
+{
+	cv::Mat frame(h * cellSize, w * cellSize, CV_8UC3,
+		      cv::Scalar(255, 255, 255));
+
+	for (size_t i = 0; i < h; ++i) {
+		for (size_t j = 0; j < w; ++j) {
+			cv::Point center(j * cellSize + cellSize / 2,
+					 i * cellSize + cellSize / 2);
+			int radius = cellSize / 2;
+			cv::Scalar color = stateToColor(grid[i * w + j]);
+			cv::circle(frame, center, radius, color, cv::FILLED);
+		}
+	}
+
+	return frame;
+}
+
+int create_video(int **grids, size_t rounds, size_t w, size_t h,
+		 const char *outputPath, int cellSize, int fps)
+{
+	if (grids == NULL) {
+		return -1;
+	}
+
+	cv::Size videoSize(w * cellSize, h * cellSize);
+
+	cv::VideoWriter writer(outputPath,
+			       cv::VideoWriter::fourcc('H', '2', '6', '4'), fps,
+			       videoSize);
+	if (!writer.isOpened()) {
+		return -1;
+	}
+
+	// Générer chaque frame
+	for (size_t i = 0; i < rounds; ++i) {
+		cv::Mat frame = createFrame(grids[i], w, h, cellSize);
+		writer.write(frame);
+	}
+
+	writer.release();
+	return 0;
+}
--- video_utils.h
+++ video_utils.h
@@ -1,19 +1,18 @@
 #ifndef VIDEO_UTILS_H
 #define VIDEO_UTILS_H
 
-#include <opencv2/opencv.hpp>
-#include <vector>
-#include <string>
+#include <stddef.h>
 
 ///@brief Creates a video from a 3D grid.
-///@param grid3D The 3D grid containing simulation steps.
-///@param outputPath The file path to save the video.
+///@param rounds a pointer containing simulation steps.
+///	  Each pointer points to w * h grid
+///@param nb_rounds The number of rounds
+///@param w the grid width
+///@param h the grid height
+///@param path The file path to save the video.
 ///@param cellSize The size of each cell in pixels.
 ///@param fps Frames per second for the video.
-///@throws std::invalid_argument if the grid3D is empty or invalid.
-///@throws std::runtime_error if the video file cannot be opened.
-void createVideo(const std::vector<std::vector<std::vector<int> > > &grid3D,
-		 const std::string &outputPath, int cellSize = 20,
-		 int fps = 10);
+int create_video(int **rounds, size_t nb_rounds, size_t w, size_t h,
+		 const char *path, int cellSize = 20, int fps = 10);
 
 #endif // VIDEO_UTILS_H
