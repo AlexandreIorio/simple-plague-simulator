6feac29ee5b6f72aa8512bfd5a5c40506e434867
André <andre_miguel_costa@hotmail.com>
2025-01-25 12:28:00 +0100

    fix: compilation errors

--- world.cu
+++ world.cu
@@ -1,9 +1,8 @@
-#include "world_priv.h"
-#define __CUDACC__
 #ifdef __CUDACC__
 
-#include "world.h"
+#include "world_priv.h"
 #include <cuda_runtime.h>
+#include <curand_kernel.h>
 #include <curand_kernel.h>
 #define CUDA_SM		 128
 #define CUDA_WARP_SIZE	 32
@@ -12,12 +11,15 @@
 #define CUDA_NB_THREAD	 (CUDA_BLOCK_DIM_X * CUDA_BLOCK_DIM_Y)
 #define CUDA_NB_BLOCK	 (CUDA_SM / CUDA_WARP_SIZE)
 
-static inline world_kernel_dimensions(const world_t* p, dim3* grid_dim, dim3* block_dim){
+static inline void world_kernel_dimensions(const world_t *p, dim3 *grid_dim,
+					   dim3 *block_dim)
+{
+	dim3 block(CUDA_BLOCK_DIM_X, CUDA_BLOCK_DIM_Y);
+	dim3 grid((p->params.worldWidth + blockDim.x - 1) / blockDim.x,
+		  (p->params.worldHeight + blockDim.y - 1) / blockDim.y);
 
-	*blockDim = (CUDA_BLOCK_DIM_X, CUDA_BLOCK_DIM_Y);
-	*gridDim = ((p->params.worldWidth + blockDim.x - 1) / blockDim.x,
-		     (p->params.worldHeight + blockDim.y - 1) / blockDim.y);
-
+	*blockDim = block;
+	*gridDim = grid;
 }
 
 static __global__ void world_init_random_values(curandState *state,
@@ -32,7 +34,7 @@ static __global__ void world_init_random_values(curandState *state,
 static inline __device__ bool should_happen(int probability,
 					    const curandState *state)
 {
-	return probability < (curand_uniform(state) % 100);
+	return probability < (curand_uniform(*state) % 100);
 }
 
 static __device__ uint8_t world_get_nb_infected_neighbours(const world_t *p,
@@ -80,23 +82,26 @@ int world_init(world_t *world, const world_parameters_t *p)
 		   CUDA_NB_THREAD * CUDA_NB_BLOCK * sizeof(*d_state));
 	dim3 grid, block;
 	world_kernel_dimensions(world, &grid, &block);
-	world_init_random_values<<<grid, block> > >(d_state, 1337);
+	world_init_random_values<<<grid, block>>>(d_state, 1337);
 	/* No need to synchronize here */
 
 	world->random_state = d_state;
-	
+
 	return 0;
 }
 
-bool __device__ world_should_infect(const world_t *p, size_t i, size_t j, int probability)
+bool __device__ world_should_infect(const world_t *p, size_t i, size_t j,
+				    int probability)
 {
 	return world_get_nb_infected_neighbours(p, i, j) &&
-	       cuda_should_happen(probability, &world->random_state[i * p->params.worldWidth + j]);
+	       should_happen(
+		       probability,
+		       &world->random_state[i * p->params.worldWidth + j]);
 }
-void __device__ world_infect_if_should_infect(const world_t *p, state_t *world, size_t i,
-					      size_t j, int probability)
+void __device__ world_infect_if_should_infect(const world_t *p, state_t *world,
+					      size_t i, size_t j,
+					      int probability)
 {
-
 	if (world_should_infect(p, state, i, j, probability)) {
 		world[i * p->params.worldWidth + j] = INFECTED;
 	}
@@ -107,7 +112,7 @@ void __device__ world_handle_infected(world_t *p, state_t *world,
 	const size_t index = i * p->params.worldWidth + j;
 
 	if (p->infectionDurationGrid[index] == 0) {
-		if (cuda_should_happen(p->params.deathProbability, state, i,
+		if (should_happen(p->params.deathProbability, state, i,
 				       j)) {
 			world[index] = DEAD;
 		} else {
@@ -155,7 +160,7 @@ void world_update(world_t *p, void *raw)
 
 	dim3 grid, block;
 	world_kernel_dimensions(world, &grid, &block);
-	world_update_k<<<grid, block> > >(d_p_in, d_tmp_world);
+	world_update_k<<<grid, block>>>(update_data->d_world, update_data->d_tmp_grid);
 
 	cudaMempcy(p->grid, update_data->d_tmp_grid, cudaMemcpyDeviceToHost);
 	cudaMempcy(p->infectionDurationGrid,
@@ -221,13 +226,12 @@ void *world_prepare_update(const world_t *p)
 
 	cuda_prepare.d_world = d_world;
 	cuda_prepare.d_tmp_grid = d_tmp_grid;
-	cuda_prepare.d_grid = d_grid;
 	cuda_prepare.d_infection_duration_grid = d_infection_duration_grid;
 
 	return (void *)&cuda_prepare;
 }
 
-void world_destroy(world *w)
+void world_destroy(world_t *w);
 {
 	cudaFree(w->random_state);
 	world_destroy_common(w);
