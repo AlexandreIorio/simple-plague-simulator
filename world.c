69024f7f6cd86202cc8b19e86b7098378b1e1ed6
André <andre_miguel_costa@hotmail.com>
2025-01-25 18:53:20 +0100

    refactor: move common world function to separate file

--- world.c
+++ world.c
@@ -14,15 +14,6 @@ static inline bool should_happen(int probability)
 	return probability < (rand() % 100);
 }
 
-static size_t get_in_state(const world_t *p, state_t state)
-{
-	const size_t size = world_world_size(&p->params);
-	size_t sum = 0;
-	for (size_t i = 0; i < size; ++i) {
-		sum += p->grid[i] == state;
-	}
-	return sum;
-}
 static uint8_t world_get_nb_infected_neighbours(const world_t *p, size_t i,
 						size_t j)
 {
@@ -50,26 +41,6 @@ static uint8_t world_get_nb_infected_neighbours(const world_t *p, size_t i,
 int world_init(world_t *world, const world_parameters_t *p)
 {
 	return world_init_common(world, p);
-}
-
-size_t world_get_infected(const world_t *p)
-{
-	return get_in_state(p, INFECTED);
-}
-
-size_t world_get_healthy(const world_t *p)
-{
-	return get_in_state(p, HEALTHY);
-}
-
-size_t world_get_immune(const world_t *p)
-{
-	return get_in_state(p, IMMUNE);
-}
-
-size_t world_get_dead(const world_t *p)
-{
-	return get_in_state(p, DEAD);
 }
 
 bool world_should_infect(const world_t *p, size_t i, size_t j, int probability)
