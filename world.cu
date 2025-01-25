af0bf67317d1fdf9d442e87617ec88812befec4d
alexandreIorio <alexandre.iorio@heig-vd.ch>
2025-01-25 22:50:33 +0100

    chore: search problem

--- world.cu
+++ world.cu
@@ -43,8 +43,7 @@ static __global__ void world_init_random_generator(curandState *state,
 static inline __device__ bool should_happen(int probability, curandState *state)
 {
 
-    unsigned long long clock_val = clock64();  // Obtenir le temps en cycles GPU
-    return = ((clock_val % 1000000) / 1000000.0f) < ((double)probability / 100) ;  // Normalisation entre 0 et 1
+return true;
 
 	// double rand_value = curand_uniform(state);
 	// return rand_value < ((double)probability / 100);
