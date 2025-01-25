e382c1c69b883ee5cd97b6aa2b2632a245bbade9
alexandreIorio <alexandre.iorio@heig-vd.ch>
2025-01-25 21:50:31 +0100

    chore: add flags

--- world.cu
+++ world.cu
@@ -172,19 +172,20 @@ void world_update(world_t *p, void *raw)
 	const size_t INFECTION_GRID_SIZE = world_size * sizeof(uint8_t);
 
 	dim3 block(CUDA_BLOCK_DIM_X, CUDA_BLOCK_DIM_Y);
+        printf("AAA\n");
 	dim3 grid((p->params.worldWidth + block.x - 1) / block.x,
 		  (p->params.worldHeight + block.y - 1) / block.y);
 	world_update_k<<<grid, block>>>(update_data->d_world,
 					  update_data->d_tmp_grid);
-    printf("AAA\n");
+    printf("BBB\n");
 	checkCudaErrors(cudaMemcpy(p->grid, update_data->d_tmp_grid, GRID_SIZE,
 				   cudaMemcpyDeviceToHost));
-    printf("BBB\n");
+
 	checkCudaErrors(cudaMemcpy(p->infectionDurationGrid,
 				   update_data->d_infection_duration_grid,
 				   INFECTION_GRID_SIZE,
 				   cudaMemcpyDeviceToHost));
-    printf("CCC\n");
+
 	cudaFree(update_data->d_tmp_grid);
 	cudaFree(update_data->d_curr_grid);
 	cudaFree(update_data->d_infection_duration_grid);
