37fa47012dac4a78e1046e62b470aea64371a88c
André <andre_miguel_costa@hotmail.com>
2025-01-26 11:08:18 +0100

    fix: remove extern C directive

--- world_priv.h
+++ world_priv.h
@@ -2,16 +2,16 @@
 #define WORLD_PRIV_H
 #include "world.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+/*#ifdef __cplusplus*/
+/*extern "C" {*/
+/*#endif*/
 
 int world_init_common(world_t *world, const world_parameters_t *p);
 size_t world_world_size(const world_parameters_t *p);
 void world_destroy_common(world_t *w);
 
-#ifdef __cplusplus
-}
-#endif
+/*#ifdef __cplusplus*/
+/*}*/
+/*#endif*/
 
 #endif
