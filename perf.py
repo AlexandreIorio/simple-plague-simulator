6063cfff5175fc501e423a9dd17858e1cc134071
André <andre_miguel_costa@hotmail.com>
2025-01-28 23:02:55 +0100

    feat: launch targets with different params and store results

--- perf.py
+++ perf.py
@@ -2,22 +2,57 @@ import subprocess
 
 TARGETS = ["std", "openmp", "cuda"]
 
+WIDTH = [int(2**i) for i in range(2, 14)]
+HEIGHT = [int(2**i) for i in range(2, 14)]
+ROUNDS = 100
+
+BENCHMARK_FILE_PATH = "benchmark_parameters.txt"
+PARAMETERS_FILE_CONTENT = """population 50
+healthy_infection_probability 5
+immune_infection_probability 0
+death_probability 10
+initial_infected 1
+initial_immune 0
+proximity 2"""
+
+BENCHMARK_RESULT_FILE = "benchmark_result.csv"
+
 
 def main():
 
-    for target in TARGETS:
+    with open(BENCHMARK_FILE_PATH, "w") as f:
+        f.write(PARAMETERS_FILE_CONTENT)
+
+    csv_lines = []
+    for target, width, height in zip(TARGETS, WIDTH, HEIGHT):
         p = subprocess.Popen(
-            [f"build/{target}/app", "-f", "parameters.txt", "-r", "100"],
+            [
+                f"build/{target}/app",
+                "-f",
+                BENCHMARK_FILE_PATH,
+                "-r",
+                str(ROUNDS),
+                "-w",
+                str(width),
+                "-h",
+                str(height),
+            ],
             stdout=subprocess.PIPE,
         )
 
         stdout, _ = p.communicate()
 
-        time_lines = stdout.decode().splitlines()[-9:-6]
+        lines = stdout.decode().splitlines()
+        time_lines = lines[-9:-6]
         times = [line.split(":")[1].replace("s", "").strip() for line in time_lines]
+        rounds = lines[-6].split(":")[1].strip()
         init, sim, total = times
+        params = (target, width, height, rounds, init, sim, total)
+        csv_lines.append(",".join(str(param) for param in params))
 
-        print(init, sim, total)
+    with open(BENCHMARK_RESULT_FILE, "w") as f:
+        f.write("target, width, height, rounds, time_init, time_simulation, time_total")
+        f.writelines(csv_lines)
 
 
 if __name__ == "__main__":
